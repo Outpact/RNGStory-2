d43f61a342fac3ffd21e65862481eb75065012f7
outi_ <outpact.2001@gmail.com>
2019-09-04 23:23:44 +1000

    Add current project

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void EndGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+}
