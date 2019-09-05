924458af1392274fef15983762612414a3790388
outi_ <outpact.2001@gmail.com>
2019-09-05 13:45:33 +1000

    Finished Start Menu

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FadeToScene (int levelIndex)
+    {
+        levelToLoad =  1;
+        animator.SetBool("Fade", true);
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/MuteVolume.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MuteVolume : MonoBehaviour, IPointerClickHandler
+{
+
+    public Sprite defaultImage;
+    public Sprite muteImage;
+
+    public bool isPressed;
+
+    public AudioSource theMusic;
+
+    private Image theSR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theSR = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!isPressed)
+        {
+            theSR.sprite = muteImage;
+            theMusic.volume = 0f;
+            Debug.Log("gay");
+            isPressed = true;
+
+        }
+        else
+        {
+            theSR.sprite = defaultImage;
+            theMusic.volume = 1f;
+            Debug.Log("gay!");
+            isPressed = false;
+        }
+    }
+}
+
